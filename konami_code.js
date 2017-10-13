a8a747c9f5ccb633308263cd5df352dd6ddc00fd
Aslan1st <aslan1st@mac.com>
2017-10-13 05:38:26 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,13 +2,12 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 var correct = []
 
 
-document.body.addEventListener('keydown', function(e){
+document.body.addEventListener('keydown', init)
+
+
+function init(e){
   const key = parseInt(e.detail || e.which)
   correct.push(key)
-  init()
-})
-
-function init(){
 
   let index = correct.length - 1
 
