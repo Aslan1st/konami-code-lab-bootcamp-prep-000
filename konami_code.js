06b51cad750b067266ba13ee7290b0b6b3d6540b
Aslan1st <aslan1st@mac.com>
2017-10-13 04:29:08 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,7 +3,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 var correct = []
 
 document.body.addEventListener('keydown', function(e){
-init()
+init(e)
 if (correct === code){
 console.log('You did it!')
 }
