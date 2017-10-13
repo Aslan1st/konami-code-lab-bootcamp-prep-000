6dcba038f9ae796eb3b48b14963d9106889d5a75
Aslan1st <aslan1st@mac.com>
2017-10-13 05:42:26 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -18,7 +18,7 @@ if ((code.length == correct.length) && code.every(function(element, index) {
     return element === correct[index];
 }))
 {
-console.log('You did it!')
+alert('You did it!')
 correct = []
   }
 }
