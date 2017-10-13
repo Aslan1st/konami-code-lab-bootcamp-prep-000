2e906e89e7104d254c1433354112d2addd3f7233
Aslan1st <aslan1st@mac.com>
2017-10-13 04:26:20 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,25 +2,15 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 var correct = []
 
-document.body.addeventlistener('keydown', function(e){
-  do {correct.push(e.which)
+document.body.addEventListener('keydown', function(e){
+init()
+if correct === code;
+console.log('You did it!')
   }
-  while (init())
-})
 
 function init() {
-
-  while (correct.length < code.length){
-    let i = 0
-    
-    if (code[i] === correct[i]){
-
-    for (i; i < correct.length; i++){
-
-      code[i] === correct[i]
-  }
-  conosole.log('You did it!')
-}  else
-  false
-}
-}
+let index = correct.length - 1
+ if (correct[index] === code[index]){
+   correct.push(e.which)
+ } else (correct = [])
+}  
